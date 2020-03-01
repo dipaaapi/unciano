9c9b0705dc5f83af5f7d37dcfc0aadc9be8291ac
John Edward Cabrera <dipaaapi8@gmail.com>
2020-03-01 15:22:04 +0800

    initial commit

---
+++ index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>OFFICIAL | UNCIANO</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="shortcut icon" href="img/unciano-logo.jpg">
+	<link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+	<nav class="navigation">
+		<div class="left">
+			<img src="img/unciano-logo.jpg" alt="brand-logo">
+		</div>
+		<div class="right">
+			<ul>
+				<li><a href="#p1"><p>HOME</p></a></li>
+				<li><a href="#p2"><p>COURSE</p></a></li>
+				<li><a href="#p3"><p>GALLERTY</p></a></li>
+				<li><p>ABOUT US</p></li>
+				<li><p>CONTACT US</p></li>
+			</ul>
+		</div>
+	</nav>
+	<div class="p1" id="p1">
+		<h1>welcome to unciano college inc. official website</h1>
+	</div>
+	<div class="p2" id="p2">
+		<h1>course</h1>
+		<div class="list">
+			<ul>
+				<li>
+					<h4>Doctor of Dental Medicine</h4>
+				</li>
+				<li>
+					<h4>B.S. of Physical Theraphy</h4>
+				</li>
+				<li>
+					<h4>Diploma in Midwifery</h4>
+				</li>
+				<li>
+					<h4>Associate of Predentistry</h4>
+				</li>
+				<li>
+					<h4>B.S. of Radiology</h4>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="p3" id="p3">
+		<h1>gallery</h1>
+	</div>
+</body>
+</html>
