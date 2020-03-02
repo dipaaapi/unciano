3d06fad0573de112c44738649f04da8559331012
John Edward Cabrera <dipaaapi8@gmail.com>
2020-03-03 01:33:03 +0800

    2nd commit

--- index.php
+++ index.php
@@ -31,24 +31,96 @@
 			<ul>
 				<li>
 					<h4>Doctor of Dental Medicine</h4>
+					<p>The Doctor of Dental Medicine is a six-year degree program in the Philippines that will train you in preventing, diagnosing, and treating oral diseases. The first phase of the program — the pre-dentistry — involves two years of taking general education courses and health-related subjects.</p>
 				</li>
 				<li>
 					<h4>B.S. of Physical Theraphy</h4>
+					<p>The Bachelor of Science in Physical Therapy (BSPT) is a five-year degree program that teaches students the knowledge and skills in the rehabilitation and treatment of individuals with disabilities resulting from injury, illness, and aging.</p>
 				</li>
 				<li>
 					<h4>Diploma in Midwifery</h4>
+					<p>The Bachelor of Science in Midwifery (BS Midwifery) is a four-year degree program in the Philippines that will train you in addressing the needs of expecting mothers and their children starting from the early stages of pregnancy up to the first few weeks following childbirth.</p>
 				</li>
 				<li>
 					<h4>Associate of Predentistry</h4>
+					<p>It aims to develop skills and technical principles of different dental procedures as well as in the prevention, diagnosis, and treatment of oral diseases. ... Those who aspire to become Dentists but graduated with a non-medical undergraduate program may enroll for the pre-dentistry course.</p>
 				</li>
 				<li>
 					<h4>B.S. of Radiology</h4>
+					<p>The Bachelor of Science in Radiologic Technology (BS RadTech) is a four-year degree program designed to train students on how to operate medical imaging equipment such as X-rays, CT scanners, MRIs, sonogram machines, and 3D medical imaging programs among other computer and web-based software programs.</p>
 				</li>
 			</ul>
 		</div>
 	</div>
 	<div class="p3" id="p3">
 		<h1>gallery</h1>
+		<div class="gallery">
+			<ul>
+				<li>
+					<div class="cont">
+						<h4 class="cont-head"></h4>
+						<img src="" alt="" class="cont-img">
+						<cont-text></cont-text>
+					</div>
+				</li>
+				<li>
+					<div class="cont">
+						<h4 class="cont-head"></h4>
+						<img src="" alt="" class="cont-img">
+						<cont-text></cont-text>
+					</div>
+				</li>
+				<li>
+					<div class="cont">
+						<h4 class="cont-head"></h4>
+						<img src="" alt="" class="cont-img">
+						<cont-text></cont-text>
+					</div>
+				</li>
+				<li>
+					<div class="cont">
+						<h4 class="cont-head"></h4>
+						<img src="" alt="" class="cont-img">
+						<cont-text></cont-text>
+					</div>
+				</li>
+				<li>
+					<div class="cont">
+						<h4 class="cont-head"></h4>
+						<img src="" alt="" class="cont-img">
+						<cont-text></cont-text>
+					</div>
+				</li>
+				<li>
+					<div class="cont">
+						<h4 class="cont-head"></h4>
+						<img src="" alt="" class="cont-img">
+						<cont-text></cont-text>
+					</div>
+				</li>
+				<li>
+					<div class="cont">
+						<h4 class="cont-head"></h4>
+						<img src="" alt="" class="cont-img">
+						<cont-text></cont-text>
+					</div>
+				</li>
+				<li>
+					<div class="cont">
+						<h4 class="cont-head"></h4>
+						<img src="" alt="" class="cont-img">
+						<cont-text></cont-text>
+					</div>
+				</li>
+				<li>
+					<div class="cont">
+						<h4 class="cont-head"></h4>
+						<img src="" alt="" class="cont-img">
+						<cont-text></cont-text>
+					</div>
+				</li>
+			</ul>
+		</div>
 	</div>
 </body>
 </html>
